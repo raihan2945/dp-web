c7972f7f8e561dbc3debb91c81ff7a1706c079d4
000raihan <000raihanahmad@gmail.com>
2023-06-10 11:45:31 +0600

    fix:icon issue solved

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -3,6 +3,8 @@ import ModalUI from "../UI/Modal";
 
 const ContactUs = ({ data }) => {
   const [modalShow, setModalShow] = useState(false);
+
+
   return (
     <>
       <div id="contact" className="container">
@@ -18,9 +20,11 @@ const ContactUs = ({ data }) => {
             <h1 className="">
               <span className="">Contact for consultation</span>
             </h1>
-            <p className="">
-              Electronic commerce draws on technologies such as mobile <br />
-              Products on online services or over the Internet.
+            <p className="" style={{padding:"0rem 1rem"}}>
+              {/* Electronic commerce draws on technologies such as mobile <br />
+              Products on online services or over the Internet. */}
+
+              To get medical advice or consultations, simply reach out to <span style={{fontWeight:700}}>{data?.Title} {data?.Name}</span> 
             </p>
             <div className="btn-container">
               <button style={{ border: "none", borderRadius: "7px" }} href="" onClick={() => setModalShow(true)} className="btn1">
