bdcd97525db91d0d3d8769c0a5230b044ef96880
namkazt <nam.kazt.91@gmail.com>
2023-08-11 13:29:49 +0700

    chore: remove unuse

--- src/components/BlogFeeds/BlogFeeds.js
+++ src/components/BlogFeeds/BlogFeeds.js
@@ -157,25 +157,7 @@ const BlogFeedItem = ({ item }) => {
     )
   }
   return (
-    <div class="card">
-      <div class="card__header">
-        <img src="https://source.unsplash.com/600x400/?computer" alt="card__image" class="card__image" width="600"></img>
-      </div>
-      <div class="card__body">
-        <span class="tag tag-blue">Technology</span>
-        <h4>What's new in 2022 Tech</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!</p>
-      </div>
-      <div class="card__footer">
-        <div class="user">
-          <img src="https://i.pravatar.cc/40?img=1" alt="user__image" class="user__image"></img>
-          <div class="user__info">
-            <h5>Jane Doe</h5>
-            <small>2h ago</small>
-          </div>
-        </div>
-      </div>
-    </div>
+    <></>
   )
 }
 
